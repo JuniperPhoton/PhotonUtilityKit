a7b8fbf32647e08269863a85fa84abd65b1f26f2
juniperphoton <dengweichao@hotmail.com>
2022-12-29 15:04:22 +0800

    Add device compat

---
+++ Sources/MyerLib/DeviceCompat.swift
@@ -0,0 +1,34 @@
+//
+//  DeviceCompat.swift
+//  MyerList
+//
+//  Created by Photon Juniper on 2022/12/13.
+//
+
+import Foundation
+
+#if canImport(UIKit)
+import UIKit
+#endif
+
+public class DeviceCompat {
+    private init() {
+        // private
+    }
+    
+    public static func isOnPhoneOnly() -> Bool {
+#if os(iOS)
+        return UIDevice.current.userInterfaceIdiom == .phone
+#else
+        return false
+#endif
+    }
+    
+    public static func isiOS() -> Bool {
+#if os(iOS)
+        return true
+#else
+        return false
+#endif
+    }
+}
