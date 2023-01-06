a9c28e9ff66b8ea49439f828d6068569ce085a9e
juniperphoton <dengweichao@hotmail.com>
2023-01-06 22:09:12 +0800

    Add isImage support to URL extensions

--- Sources/MyerLib/URLExtensions.swift
+++ Sources/MyerLib/URLExtensions.swift
@@ -6,9 +6,36 @@
 //
 
 import Foundation
+import UniformTypeIdentifiers
 
 public extension URL {
     var isDir: Bool {
         (try? resourceValues(forKeys: [.isDirectoryKey]))?.isDirectory == true
+    }
+    
+    func isImage() -> Bool {
+        guard let typeID = try? self.resourceValues(forKeys: [.typeIdentifierKey]).typeIdentifier else {
+            return false
+        }
+        
+        let utType = UTType(typeID)
+        
+        if let supertypes = utType?.supertypes {
+            if supertypes.contains(.image) {
+                return true
+            }
+        }
+        
+        // We check the UTType of the content to avoid loading data for non-image file
+        if utType != .tiff
+            && utType != .jpeg
+            && utType != .png
+            && utType != .heic
+            && utType != .rawImage
+            && utType?.identifier != "com.adobe.raw-image" {
+            return false
+        }
+        
+        return true
     }
 }
