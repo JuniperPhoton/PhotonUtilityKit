088215dc54e5360be390f0bb6b89b067550044d9
juniperphoton <dengweichao@hotmail.com>
2023-01-07 19:59:36 +0800

    Optimize page view

--- Sources/MyerView/PageView.swift
+++ Sources/MyerView/PageView.swift
@@ -8,15 +8,31 @@
 import SwiftUI
 import MyerLib
 
+public struct PagingTranslation: CustomStringConvertible, Equatable {
+    let currentIndex: Int
+    let nextIndex: Int
+    let progress: CGFloat
+    
+    public init(currentIndex: Int, nextIndex: Int, progress: CGFloat) {
+        self.currentIndex = currentIndex
+        self.nextIndex = nextIndex
+        self.progress = progress
+    }
+    
+    public var description: String {
+        return "\(currentIndex) -> \(nextIndex), Progress: \(progress)"
+    }
+}
+
 /// Current not used.
 public struct PageView<Content: View, C: RandomAccessCollection>: View where C.Element: Identifiable & Equatable {
     let items: C
     let itemContent: (C.Element) -> Content
-    let startPageIndex: Int
+    let pageIndex: Binding<Int>
     
     let disablePaging: Binding<Bool>
     
-    var onPageIndexChanged: ((Int) -> Void)? = nil
+    var onPageTranslationChanged: ((PagingTranslation) -> Void)? = nil
     
     @GestureState var dragTranslationX: CGFloat = 0
     
@@ -28,7 +44,7 @@ public struct PageView<Content: View, C: RandomAccessCollection>: View where C.E
             if let originalIndex = items.firstIndex(where: { v in
                 v.id == displayedItemId
             }) as? Int {
-                onPageIndexChanged?(originalIndex)
+                pageIndex.wrappedValue = originalIndex
             }
         }
     }
@@ -36,19 +52,30 @@ public struct PageView<Content: View, C: RandomAccessCollection>: View where C.E
     @State var displayedItems: [C.Element] = []
     @State var width: CGFloat = 0.0
     
-    private let offscreenCountPerSide = 1
-    private let spacing: CGFloat = 20
+    private let offscreenCountPerSide: Int
+    private let spacing: CGFloat
+    private let scrollSlop: CGFloat
+    private let animationDuration: CGFloat
     
-    public init(items: C, startPageIndex: Int,
+    public init(items: C,
+                pageIndex: Binding<Int>,
                 disablePaging: Binding<Bool>,
-                onPageIndexChanged: ((Int) -> Void)? = nil,
+                offscreenCountPerSide: Int = 2,
+                spacing: CGFloat = 20,
+                scrollSlop: CGFloat = 20,
+                animationDuration: CGFloat = 0.3,
+                onPageTranslationChanged: ((PagingTranslation) -> Void)? = nil,
                 @ViewBuilder itemContent: @escaping (C.Element) -> Content) {
         self.items = items
         self.itemContent = itemContent
-        self.startPageIndex = startPageIndex
+        self.pageIndex = pageIndex
         self.disablePaging = disablePaging
-        self.onPageIndexChanged = onPageIndexChanged
-        print("dwccc start page index \(startPageIndex), disable paging \(disablePaging)")
+        self.offscreenCountPerSide = offscreenCountPerSide
+        self.spacing = spacing
+        self.scrollSlop = scrollSlop
+        self.animationDuration = animationDuration
+        self.onPageTranslationChanged = onPageTranslationChanged
+        print("dwccc start page index \(pageIndex), disable paging \(disablePaging)")
     }
     
     private func calculateDisplayItems(originalIndex: Int) {
@@ -79,7 +106,6 @@ public struct PageView<Content: View, C: RandomAccessCollection>: View where C.E
             .updating($dragTranslationX) { v, state, _ in
                 state = v.translation.width
             }
-        
         GeometryReader { proxy in
             HStack(spacing: spacing) {
                 ForEach(displayedItems, id: \.id) { item in
@@ -93,51 +119,72 @@ public struct PageView<Content: View, C: RandomAccessCollection>: View where C.E
         .contentShape(Rectangle())
         .simultaneousGesture(gesture, including: disablePaging.wrappedValue ? .subviews : .all)
         .onAppear {
-            calculateDisplayItems(originalIndex: startPageIndex)
-            updateVirtualPageIndex(originalIndex: startPageIndex)
+            calculateDisplayItems(originalIndex: pageIndex.wrappedValue)
+            updateVirtualPageIndex(originalIndex: pageIndex.wrappedValue)
         }
         .onChange(of: dragTranslationX) { newValue in
             translationX = newValue
+            
+            let progress: CGFloat = abs(translationX) / self.width
+            if progress == 0 {
+                return
+            }
+            
+            let currentIndex = pageIndex.wrappedValue
+            var nextIndex = translationX < 0 ? currentIndex + 1 : currentIndex - 1
+            nextIndex = nextIndex.clamp(to: 0...items.count - 1)
+            
+            
+            let translation = PagingTranslation(currentIndex: currentIndex,
+                                                nextIndex: nextIndex, progress: progress)
+            self.onPageTranslationChanged?(translation)
         }
+        .onChange(of: pageIndex.wrappedValue, perform: { newValue in
+            withEastOutAnimation(duration: animationDuration) {
+                updateVirtualPageIndex(originalIndex: newValue)
+            }
+        })
         .listenWidthChanged { width in
             self.width = width
         }
     }
     
     private func onGestureEnd(value: DragGesture.Value) {
-        let duration = 0.3
-        let scrollSlop: CGFloat = 20
-        
-        withEastOutAnimation(duration: duration) {
-            var newIndex = virtualPageIndex
+        withEastOutAnimation(duration: animationDuration) {
+            var newVirtualIndex = virtualPageIndex
             if translationX > scrollSlop {
                 var index = virtualPageIndex - 1
                 if index < 0 {
                     index = 0
                 }
-                newIndex = index
+                newVirtualIndex = index
             } else if translationX < -scrollSlop {
                 var index = virtualPageIndex + 1
                 if index >= CGFloat(displayedItems.count) {
                     index = CGFloat(displayedItems.count) - 1
                 }
-                newIndex = index
+                newVirtualIndex = index
             }
             
-            print("dwccc on gesture end newIndex: \(newIndex), from virtualPageIndex \(virtualPageIndex)")
+            print("dwccc on gesture end newVirtualIndex: \(newVirtualIndex), from virtualPageIndex \(virtualPageIndex)")
             
-            self.virtualPageIndex = newIndex
+            self.virtualPageIndex = newVirtualIndex
             self.translationX = 0
             
-            DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
-                let currentItem = displayedItems[Int(self.virtualPageIndex)]
-                let originalIndex = items.firstIndex { id in
-                    id == currentItem
-                } as! Int
-                
-                withEastOutAnimation(duration: duration) {
-                    calculateDisplayItems(originalIndex: originalIndex)
-                    updateVirtualPageIndex(originalIndex: originalIndex)
+            let currentItem = displayedItems[Int(self.virtualPageIndex)]
+            let nextOriginalIndex = items.firstIndex { id in
+                id == currentItem
+            } as! Int
+            
+            let translation = PagingTranslation(currentIndex: pageIndex.wrappedValue,
+                                                nextIndex: nextOriginalIndex, progress: 1.0)
+            
+            self.onPageTranslationChanged?(translation)
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + animationDuration) {
+                withEastOutAnimation(duration: animationDuration) {
+                    calculateDisplayItems(originalIndex: nextOriginalIndex)
+                    updateVirtualPageIndex(originalIndex: nextOriginalIndex)
                 }
             }
         }
