157578a11713b470659fa75cccd560efcb526e6d
juniperphoton <dengweichao@hotmail.com>
2023-01-08 15:16:57 +0800

    Add animateBackForth

--- Sources/MyerLib/AppAnimation.swift
+++ Sources/MyerLib/AppAnimation.swift
@@ -8,10 +8,45 @@
 import Foundation
 import SwiftUI
 
-public func withEastOutAnimation<Result>(duration: Double = 0.3,
-                                         _ delay: Double = 0.0,
+/// Returns the result of recomputing the view's body with the provided
+/// animation.
+///
+/// The animation is fixed to using eastOut curve. And you can custom the ``duration``, ``delay`` of this animation.
+/// To perform task on the animation ended, you pass the ``onEnd`` to achive that. You use the ``onEndDelay`` to control how ``onEnd`` will be invoked
+/// after the original animation started.
+///
+/// If you want to animate value from one to other, then from the other to another one, you can use the ``animate(value:from:to:duration:_:interval:)`` for convenience.
+public func withEastOutAnimation<Result>(duration: TimeInterval = 0.3,
+                                         _ delay: TimeInterval = 0.0,
+                                         onEnd: (() -> Void)? = nil,
+                                         onEndDelay: TimeInterval = 0.3,
                                          _ body: () throws -> Result) -> Result? {
-    return try? withAnimation(Animation.easeOut(duration: duration).delay(delay)) {
+    let result = try? withAnimation(Animation.easeOut(duration: duration).delay(delay)) {
         try body()
+    }
+    if let onEnd = onEnd {
+        DispatchQueue.main.asyncAfter(wallDeadline: .now() + duration + delay + onEndDelay) {
+            withAnimation(Animation.easeOut(duration: duration).delay(delay)) {
+                onEnd()
+            }
+        }
+    }
+    return result
+}
+
+/// Animate the binding ``value`` from the ``from`` value to ``to`` value and wait for ``internal`` and reverse back.
+///
+/// You can custom the animation by setting the ``duration`` and ``delay``.
+public func animateBackForth<T>(value: Binding<T>,
+                                from: T, to: T?,
+                                duration: TimeInterval = 0.3,
+                                _ delay: TimeInterval = 0.0,
+                                interval: TimeInterval = 1.0) {
+    withEastOutAnimation(duration: duration, delay, onEnd: {
+        if to != nil {
+            value.wrappedValue = to!
+        }
+    }, onEndDelay: interval) {
+        value.wrappedValue = from
     }
 }
