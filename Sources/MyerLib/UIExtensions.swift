e67d008246095861d6e53325a3cc4478a7e64510
juniperphoton <dengweichao@hotmail.com>
2023-02-06 12:44:37 +0800

    Support init color by hex string

--- Sources/MyerLib/UIExtensions.swift
+++ Sources/MyerLib/UIExtensions.swift
@@ -54,6 +54,38 @@ public extension View {
 }
 
 public extension Color {
+    init?(hexString: String?) {
+        guard let hexString = hexString else {
+            return nil
+        }
+        
+        if !hexString.hasPrefix("#") {
+            return nil
+        }
+        
+        let start = hexString.index(hexString.startIndex, offsetBy: 1)
+        let hexColor = String(hexString[start...])
+        
+        let scanner = Scanner(string: hexColor)
+        var hexNumber: UInt64 = 0
+        
+        if scanner.scanHexInt64(&hexNumber) {
+            let a: CGFloat
+            if hexColor.count == 8 {
+                a = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+            } else {
+                a = 1.0
+            }
+            let r = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+            let g = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+            let b = CGFloat(hexNumber & 0x000000ff) / 255
+            
+            self = Color.init(red: r, green: g, blue: b, opacity: a)
+        } else {
+            return nil
+        }
+    }
+    
     init(hex: UInt, alpha: Double = 1) {
         self.init(
             .sRGB,
