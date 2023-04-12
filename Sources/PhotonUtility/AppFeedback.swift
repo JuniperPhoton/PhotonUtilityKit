df9932f60bfff2e13223e59742615bd074906242
juniperphoton <dengweichao@hotmail.com>
2023-04-12 13:05:50 +0800

    Add getOSNameVersion

--- Sources/PhotonUtility/AppFeedback.swift
+++ Sources/PhotonUtility/AppFeedback.swift
@@ -7,6 +7,12 @@
 
 import Foundation
 
+#if canImport(AppKit)
+import AppKit
+#elseif canImport(UIKit)
+import UIKit
+#endif
+
 /// Get a ``URL`` to send feedback to, with ``subject`` and ``body``.
 public func getFeedbackURL(subject: String, body: String) -> URL? {
     var component = URLComponents(string: "mailto:dengweichao@hotmail.com")
@@ -15,4 +21,21 @@ public func getFeedbackURL(subject: String, body: String) -> URL? {
         URLQueryItem(name: "body", value: body)
     ]
     return component?.url
+}
+
+/// Get the full os version string with os name, like iOS 16.
+public func getOSNameVersion() -> String {
+    #if os(iOS)
+    let osVersion = UIDevice.current.systemVersion
+    return "iOS \(String(osVersion))"
+    #elseif os(macOS)
+    let os = ProcessInfo.processInfo.operatingSystemVersion
+    let osVersion = "\(os.majorVersion).\(os.minorVersion).\(os.patchVersion)"
+    return "macOS \(String(osVersion))"
+    #elseif os(tvOS)
+    let osVersion = UIDevice.current.systemVersion
+    return "tvOS \(String(osVersion))"
+    #else
+    return ""
+    #endif
 }
