cdf2593fc54d91f3bcbdcdc5873f2ce812f157ee
dengweichao <dengweichao@hotmail.com>
2023-06-24 19:47:45 +0800

    Add demo project

---
+++ PhotonUtilityKitDemo/PhotonUtilityKitDemo/PhotonUtilityKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotonUtilityKitDemoApp.swift
+//  PhotonUtilityKitDemo
+//
+//  Created by Photon Juniper on 2023/6/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotonUtilityKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
