37025dd04b56b9ab3ced01b493e2eac3d25ff605
dengweichao <dengweichao@hotmail.com>
2023-07-31 22:39:53 +0800

    Add code showcases in UnevenRoundedRectDemoView

--- PhotonUtilityKitDemo/PhotonUtilityKitDemo/View/Details/UnevenRoundedRectDemoView.swift
+++ PhotonUtilityKitDemo/PhotonUtilityKitDemo/View/Details/UnevenRoundedRectDemoView.swift
@@ -5,17 +5,30 @@
 //  Created by Photon Juniper on 2023/6/25.
 //
 
-import SwiftUI
 import PhotonUtility
 import PhotonUtilityView
+import SwiftUI
 
 struct UnevenRoundedRectDemoView: View {
+    private let code = HighliableCode(code: """
+    Text("UnevenRoundedRectangleView")
+        .padding()
+        .background(
+            UnevenRoundedRectangle(top: 12, bottom: 0).fill(.gray.opacity(0.1))
+        )
+    """)
+
     var body: some View {
         VStack {
+            HighliableCodeView(code: code)
+            
             Text("UnevenRoundedRectangleView")
                 .padding()
-                .background(UnevenRoundedRectangle(top: 12, bottom: 0).fill(.gray.opacity(0.1)))
+                .background(
+                    UnevenRoundedRectangle(top: 12, bottom: 0).fill(.gray.opacity(0.1))
+                )
         }
+        .padding()
         .matchParent()
         .navigationTitle("UnevenRoundedRect")
     }
