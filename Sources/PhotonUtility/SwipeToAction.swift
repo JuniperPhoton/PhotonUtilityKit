7b07db664930bc0ee3401669b43ff984a4be4e90
dengweichao <dengweichao@hotmail.com>
2023-08-09 17:58:55 +0800

    Optimize swipeToAction implementation

--- Sources/PhotonUtility/SwipeToAction.swift
+++ Sources/PhotonUtility/SwipeToAction.swift
@@ -18,17 +18,13 @@ public struct SwipeToAction: ViewModifier {
     
     private(set) var thresholdToAction: CGFloat
     private(set) var onAction: (Axis) -> Bool
-    private(set) var onTranslationXChanged: ((CGFloat) -> Void)? = nil
     private(set) var onEnd: (() -> Void)? = nil
     
     private(set) var axisMask = Axis.leadingToTrailing
     
     @State private var viewWidth: CGFloat = 0
-    @State private var translationX: CGFloat = 0 {
-        didSet {
-            onTranslationXChanged?(translationX)
-        }
-    }
+    @Binding var translationX: CGFloat
+    
     @GestureState private var dragStateOffsetX: CGFloat = 0
     
     /// Init this view with:
@@ -38,50 +34,50 @@ public struct SwipeToAction: ViewModifier {
     /// - Parameter onEnd: the block to be invoked when the gesture is ended
     init(axisMask: Axis,
          thresholdToAction: CGFloat,
-         onTranslationXChanged: ((CGFloat) -> Void)? = nil,
+         translationX: Binding<CGFloat>,
          onAction: @escaping (Axis) -> Bool,
          onEnd: (() -> Void)? = nil) {
         self.axisMask = axisMask
         self.thresholdToAction = thresholdToAction
         self.onAction = onAction
-        self.onTranslationXChanged = onTranslationXChanged
+        self._translationX = translationX
         self.onEnd = onEnd
     }
     
     public func body(content: Content) -> some View {
-        ZStack {
-            content
-                .offset(x: translationX)
-                .listenWidthChanged(onWidthChanged: { width in
+        content
+            .offset(x: translationX)
+            .listenWidthChanged { width in
+                if self.viewWidth != width {
                     self.viewWidth = width
-                })
+                }
+            }
 #if !os(tvOS)
-                .gesture(DragGesture().updating($dragStateOffsetX) { value, state, transaction in
-                    // Updating method would always be invoked even is cancelled by system.
-                    // We depend the state here to update the translationX state
-                    state = value.translation.width
-                }, including: .all)
+            .gesture(DragGesture().updating($dragStateOffsetX) { value, state, transaction in
+                // Updating method would always be invoked even is cancelled by system.
+                // We depend the state here to update the translationX state
+                state = value.translation.width
+            }, including: .all)
 #endif
-                .matchParent(axis: .width, alignment: .leading)
-                .onChange(of: dragStateOffsetX) { newValue in
-                    // If the value is changed to zero, then trigger the end of gesture
-                    if newValue == 0 {
-                        triggerEnd()
-                    } else {
-                        if axisMask == .leadingToTrailing {
-                            if newValue > 0 {
-                                translationX = newValue
-                            }
-                        } else if axisMask == .trailingToLeading {
-                            if newValue < 0 {
-                                translationX = newValue
-                            }
-                        } else {
+            .matchParent(axis: .width, alignment: .leading)
+            .onChange(of: dragStateOffsetX) { newValue in
+                // If the value is changed to zero, then trigger the end of gesture
+                if newValue == 0 {
+                    triggerEnd()
+                } else {
+                    if axisMask == .leadingToTrailing {
+                        if newValue > 0 {
                             translationX = newValue
                         }
+                    } else if axisMask == .trailingToLeading {
+                        if newValue < 0 {
+                            translationX = newValue
+                        }
+                    } else {
+                        translationX = newValue
                     }
                 }
-        }
+            }
     }
     
     private func triggerEnd() {
@@ -123,14 +119,20 @@ public extension View {
     /// - Parameter onTranslationXChanged: the block to be invoked on the translation x of the view is changed
     /// - Parameter onAction: the block to be invoked when the swipe translation is greater than the thresholdToAction in points. Return true will restore  the translationx on action performed.
     /// - Parameter onEnd: the block to be invoked when the gesture is ended
-    func swipeToAction(axisMask: SwipeToAction.Axis = .leadingToTrailing,
+    @ViewBuilder
+    func swipeToAction(enabled: Bool = true,
+                       axisMask: SwipeToAction.Axis = .leadingToTrailing,
                        thresholdToAction: CGFloat = 100,
-                       onTranslationXChanged: ((CGFloat) -> Void)? = nil,
+                       translationX: Binding<CGFloat>,
                        onEnd: (() -> Void)? = nil,
                        onAction: @escaping (SwipeToAction.Axis) -> Bool) -> some View {
-        self.modifier(SwipeToAction(axisMask: axisMask,
-                                    thresholdToAction: thresholdToAction,
-                                    onTranslationXChanged: onTranslationXChanged,
-                                    onAction: onAction, onEnd: onEnd))
+        if !enabled {
+            self
+        } else {
+            self.modifier(SwipeToAction(axisMask: axisMask,
+                                        thresholdToAction: thresholdToAction,
+                                        translationX: translationX,
+                                        onAction: onAction, onEnd: onEnd))
+        }
     }
 }
