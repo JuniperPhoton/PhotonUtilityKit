9f668ca08ea5ebbb8d8c88bbb5b947c076670684
dengweichao <dengweichao@hotmail.com>
2023-11-01 17:44:12 +0800

    Make call invokeOnDismiss when dismissing all in FullscrenPresentation

--- Sources/PhotonUtilityView/FullscreenPresentation.swift
+++ Sources/PhotonUtilityView/FullscreenPresentation.swift
@@ -74,6 +74,7 @@ public class FullscreenPresentation: ObservableObject {
         withTransaction(createTranscation(animated: animated)) {
             self.presentedView = nil
             self.transition = transition
+            self.invokeOnDismiss()
         }
     }
     
