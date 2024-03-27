bf5e2de146df4699822b719e5c8e792de5b2b665
juniperphoton <dengweichao@hotmail.com>
2024-03-27 13:50:49 +0800

    Add more methods in CIImageExtensions

--- Sources/PhotonUtility/Graphics/CIImageExtensions.swift
+++ Sources/PhotonUtility/Graphics/CIImageExtensions.swift
@@ -1,13 +1,70 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Photon Juniper on 2024/3/27.
 //
 import Foundation
 import CoreImage
+import CoreImage.CIFilterBuiltins
+
+extension CIImage {
+    /// Get the color inverted image.
+    func colorInverted() -> CIImage? {
+        return self.applyFilter(CIFilter.colorInvert())
+    }
+    
+    /// Get the color clamped image.
+    func colorClamped() -> CIImage? {
+        return self.applyFilter(CIFilter.colorClamp())
+    }
+    
+    /// Apply a filter, configure the parameters of this filter and return the output image.
+    /// This method provides an easy way to chain your filter.
+    ///
+    /// Given an input image, to apply a filter:
+    ///
+    /// ```swift
+    /// let outputImage = inputImage.applyFilter(CIFilter.colorMatrix()) { filter in
+    ///     // Configure colorMatrix filter.
+    ///     // Input image will be set automatically.
+    /// }
+    /// ```
+    ///
+    /// - clampedToExtent: Convenient option to set the input clampedToExtent and then crop the output to the original extent.
+    func applyFilter<T: CIFilter>(
+        _ filter: T,
+        clampedToExtent: Bool = false,
+        transform: ((T) -> Void)? = nil
+    ) -> CIImage? {
+        let filter = filter
+        transform?(filter)
+        
+        var input = self
+        
+        let originalExtent = self.extent
+        if clampedToExtent {
+            input = self.clampedToExtent()
+        }
+        
+        filter.setValue(input, forKey: kCIInputImageKey)
+        
+        let output = filter.outputImage
+        if clampedToExtent {
+            return output?.cropped(to: originalExtent)
+        } else {
+            return output
+        }
+    }
+}
 
 public extension CIImage {
+    /// Get the underlaying pixelBuffer of this ``CIImage`` or create a new one to a preferredFormat.
+    ///
+    /// If you would like to always create a new ``CVPixelBuffer``, use ``createCVPixelBuffer(ciContext:preferredFormat:)``.
+    ///
+    /// - parameter ciContext: The CIContext to use when creating a new ``CVPixelBuffer``.
+    /// - parameter preferredFormat: The preferred pixel format when creating a new ``CVPixelBuffer``.
     func getOrCreateCVPixelBuffer(
         ciContext: CIContext? = nil,
         preferredFormat: OSType = kCVPixelFormatType_32BGRA
@@ -22,6 +79,9 @@ public extension CIImage {
         }
     }
     
+    /// Create a new ``CVPixelBuffer`` from this image.
+    /// - parameter ciContext: The CIContext to use.
+    /// - parameter preferredFormat: The preferred pixel format.
     func createCVPixelBuffer(
         ciContext: CIContext? = nil,
         preferredFormat: OSType = kCVPixelFormatType_32BGRA
