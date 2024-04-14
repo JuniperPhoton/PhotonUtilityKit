3ed0ef62b8245db8d5f8858fa651fb0509e2e9e5
juniperphoton <dengweichao@hotmail.com>
2024-04-15 01:18:06 +0800

    Add EasingFunctions

---
+++ Sources/PhotonUtility/EasingFunctions.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//
+//
+//  Created by Photon Juniper on 2024/4/15.
+//
+
+import Foundation
+
+/// Provides some easing functions from https://easings.net.
+public class EasingFunctions {
+    /// https://easings.net/#easeInOutQuint
+    static public func easeInOutQuint(_ x: CGFloat) -> CGFloat {
+        return x < 0.5 ? 16 * x * x * x * x * x : 1 - pow(-2 * x + 2, 5) / 2
+    }
+    
+    /// https://easings.net/#easeOutQuint
+    static public func easeOutQuint(_ x: CGFloat) -> CGFloat {
+        return 1 - pow(1 - x, 5)
+    }
+    
+    /// https://easings.net/#easeInQuint
+    static public func easeInQuint(_ x: CGFloat) -> CGFloat {
+        return x * x * x * x * x
+    }
+}
