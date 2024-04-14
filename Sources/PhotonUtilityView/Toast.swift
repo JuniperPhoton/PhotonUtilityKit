4a5088780a133eade03d4b21e0a19984f1c99ae6
juniperphoton <dengweichao@hotmail.com>
2024-04-15 03:59:26 +0800

    Fix build break for tvOS

--- Sources/PhotonUtilityView/Toast.swift
+++ Sources/PhotonUtilityView/Toast.swift
@@ -90,7 +90,7 @@ public enum ToastShape {
     case roundedRect(CGFloat)
     case capsule
     
-    @available(iOS 16.0, macOS 13.0, *)
+    @available(iOS 16.0, macOS 13.0, tvOS 16.0, *)
     var toShape: AnyShape {
         switch self {
         case .roundedRect(let radius):
@@ -222,7 +222,7 @@ fileprivate struct ToastContentView: View {
                 .multilineTextAlignment(.center)
         }.padding(EdgeInsets(top: 8, leading: 12, bottom: 8, trailing: 12))
             .background(Group {
-                if #available(iOS 16.0, macOS 13.0, *) {
+                if #available(iOS 16.0, macOS 13.0, tvOS 16.0, *) {
                     style.shape.toShape.fill(colors.backgroundColor)
                 } else {
                     switch style.shape {
