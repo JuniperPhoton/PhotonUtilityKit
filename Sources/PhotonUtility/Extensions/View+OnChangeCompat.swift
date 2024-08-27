07e90d7d09a5c04bffd1d1b322a30fa450b764b6
juniperphoton <dengweichao@hotmail.com>
2024-08-28 02:09:30 +0800

    Add View+OnChangeCompat

---
+++ Sources/PhotonUtility/Extensions/View+OnChangeCompat.swift
@@ -0,0 +1,35 @@
+//
+//  OnChangeCompat.swift
+//  PhotonCam
+//
+//  Created by Photon Juniper on 2024/8/28.
+//
+import SwiftUI
+
+extension View {
+    /// A compat version of ``View/onChange(of:perform:)`` that takes single parameter.
+    @ViewBuilder
+    func onChangeCompat<V: Equatable>(of value: V, perform: @escaping (_ newValue: V) -> Void) -> some View {
+        if #available(iOS 17.0, *) {
+            self.onChange(of: value) { _, newValue in
+                perform(newValue)
+            }
+        } else {
+            self.onChange(of: value, perform: perform)
+        }
+    }
+    
+    /// A compat version of ``View/onChange(of:perform:)`` that takes zero parameter.
+    @ViewBuilder
+    func onChangeCompat<V: Equatable>(of value: V, perform: @escaping () -> Void) -> some View {
+        if #available(iOS 17.0, *) {
+            self.onChange(of: value) { _, _ in
+                perform()
+            }
+        } else {
+            self.onChange(of: value) { _ in
+                perform()
+            }
+        }
+    }
+}
