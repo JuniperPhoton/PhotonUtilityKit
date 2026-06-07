699a3e4dc6190ae1d51129e26b31dd1274b40795
JuniperPhoton <dengweichao@hotmail.com>
2026-06-08 02:03:39 +0900

    Support liquid glass for toast

--- Sources/PhotonUtilityView/Toast.swift
+++ Sources/PhotonUtilityView/Toast.swift
@@ -223,7 +223,8 @@ public struct ToastContentView: View {
 }
 
 fileprivate struct ToastContentViewInternal: View {
-    @Environment(\.toastStyle) private var style: ToastStyle
+    @Environment(\.toastStyle) private var style
+    @Environment(\.toastUserLiquidGlassStyleIfAvailable) private var toastUserLiquidGlassStyleIfAvailable
     
     @State var showBellAnimation = false
     
@@ -240,13 +241,27 @@ fileprivate struct ToastContentViewInternal: View {
                     .animation(.default.repeatForever(), value: showBellAnimation)
             }
             Text(LocalizedStringKey(toast))
-                .foregroundColor(colors.foregroundColor)
                 .multilineTextAlignment(.center)
+                .liquidGlassIfAvailable { v in
+                    if #available(iOS 26.0, *), toastUserLiquidGlassStyleIfAvailable {
+                        v
+                    } else {
+                        v.foregroundColor(colors.foregroundColor)
+                    }
+                } fallback: { v in
+                    v.foregroundColor(colors.foregroundColor)
+                }
         }.padding(EdgeInsets(top: 8, leading: 12, bottom: 8, trailing: 12))
             .clipShape(Rectangle())
-            .background(Group {
-                style.shape.toShape.fill(colors.backgroundColor)
-            }.addShadow())
+            .liquidGlassIfAvailable { v in
+                if #available(iOS 26.0, *), toastUserLiquidGlassStyleIfAvailable {
+                    v.glassEffect(.regular.interactive(), in: style.shape.toShape)
+                } else {
+                    v.background(style.shape.toShape.fill(colors.backgroundColor).addShadow())
+                }
+            } fallback: { v in
+                v.background(style.shape.toShape.fill(colors.backgroundColor).addShadow())
+            }
             .padding(8)
             .animation(.default, value: style.showIcon)
             .applyContentTransitionIfAvailable()
@@ -265,6 +280,10 @@ public extension View {
     func toast(text: Binding<String>) -> some View {
         self.modifier(ToastModifier(toast: text))
     }
+}
+
+public extension EnvironmentValues {
+    @Entry var toastUserLiquidGlassStyleIfAvailable = true
 }
 
 fileprivate extension View {
